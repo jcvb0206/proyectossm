87db64155c069e7a8b0942c4d5458a855ca14e36
Joseph Vega <jcvb0206@gmail.com>
2021-03-05 22:34:03 -0500

    Add icons component

--- src/components/UI/Icon.js
+++ src/components/UI/Icon.js
@@ -1,1 +1,41 @@
-// To edit
+/** @jsxImportSource @emotion/react */
+// import styled from '@emotion/styled';
+import { css } from '@emotion/react';
+import {
+  FaFacebookMessenger,
+  FaFacebookSquare,
+  FaFacebook,
+  FaInstagram,
+  FaLinkedin,
+  FaUserCircle,
+} from 'react-icons/fa';
+import { HiOutlineMail, HiOutlineMailOpen } from 'react-icons/hi';
+import { RiHome3Line } from 'react-icons/ri';
+import { IoChevronBackOutline, IoChevronForwardOutline } from 'react-icons/io5';
+
+const iconSet = {
+  home: RiHome3Line,
+  profile: FaUserCircle,
+  back: IoChevronBackOutline,
+  forward: IoChevronForwardOutline,
+  email: HiOutlineMail,
+  emailOpen: HiOutlineMailOpen,
+  facebookSquare: FaFacebookSquare,
+  facebook: FaFacebook,
+  messenger: FaFacebookMessenger,
+  instagram: FaInstagram,
+  linkedin: FaLinkedin,
+};
+
+export default function Icon({ type, fill, size, onClick }) {
+  const IconToRender = iconSet[type];
+  return (
+    <IconToRender
+      onClick={onClick}
+      css={css`
+        fill: ${fill};
+        font-size: ${size}px;
+      `}
+    ></IconToRender>
+  );
+}
