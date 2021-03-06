d483a5f33ca32b917ed5d5429571d2182710670e
Joseph Vega <jcvb0206@gmail.com>
2021-03-05 22:52:37 -0500

    Set default color fill and stroke of icons

--- src/components/UI/Icon.js
+++ src/components/UI/Icon.js
@@ -14,26 +14,27 @@ import { RiHome3Line } from 'react-icons/ri';
 import { IoChevronBackOutline, IoChevronForwardOutline } from 'react-icons/io5';
 
 const iconSet = {
-  home: RiHome3Line,
-  profile: FaUserCircle,
-  back: IoChevronBackOutline,
-  forward: IoChevronForwardOutline,
-  email: HiOutlineMail,
-  emailOpen: HiOutlineMailOpen,
-  facebookSquare: FaFacebookSquare,
-  facebook: FaFacebook,
-  messenger: FaFacebookMessenger,
-  instagram: FaInstagram,
-  linkedin: FaLinkedin,
+  home: [RiHome3Line, 'rgb(27, 61, 94)', ''],
+  profile: [FaUserCircle, 'rgb(27, 61, 94)', ''],
+  back: [IoChevronBackOutline, '', 'rgb(27, 61, 94)'],
+  forward: [IoChevronForwardOutline, '', 'rgb(27, 61, 94)'],
+  email: [HiOutlineMail, '', 'rgb(27, 61, 94)'],
+  emailOpen: [HiOutlineMailOpen, '', 'rgb(27, 61, 94)'],
+  facebookSquare: [FaFacebookSquare, 'rgb(27, 61, 94)', ''],
+  facebook: [FaFacebook, 'rgb(27, 61, 94)', ''],
+  messenger: [FaFacebookMessenger, 'rgb(27, 61, 94)', ''],
+  instagram: [FaInstagram, 'rgb(27, 61, 94)', ''],
+  linkedin: [FaLinkedin, 'rgb(27, 61, 94)', ''],
 };
 
-export default function Icon({ type, fill, size, onClick }) {
-  const IconToRender = iconSet[type];
+export default function Icon({ type, fill, stroke, size, onClick }) {
+  const [IconToRender, iconFill, iconStroke] = iconSet[type];
   return (
     <IconToRender
       onClick={onClick}
       css={css`
-        fill: ${fill};
+        fill: ${fill || iconFill};
+        stroke: ${stroke || iconStroke};
         font-size: ${size}px;
       `}
     ></IconToRender>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,25 @@
 import styled from '@emotion/styled';
 import { css } from '@emotion/react';
+import Icon from '../components/UI/Icon';
 
 const StyledDiv = styled.div``;
 
 function Home({ history }) {
-  return <StyledDiv></StyledDiv>;
+  return (
+    <StyledDiv>
+      <Icon type="home" size="30" />
+      <Icon type="profile" size="30" />
+      <Icon type="back" size="30" />
+      <Icon type="forward" size="30" />
+      <Icon type="email" size="30" />
+      <Icon type="emailOpen" size="30" />
+      <Icon type="facebookSquare" size="30" />
+      <Icon type="facebook" size="30" />
+      <Icon type="messenger" size="30" />
+      <Icon type="instagram" size="30" />
+      <Icon type="linkedin" size="30" />
+    </StyledDiv>
+  );
 }
 
 export default Home;
